046ab856eb9e6bc2c8fb5fefe350240d74a32397
Roniere Marques <109429582+RoniereMarques@users.noreply.github.com>
2023-10-20 17:15:17 -0300

    Update code.js
    Update of code.js
    20/10/2023

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
+// Função para copiar texto para a área de transferência
+function copyToClipboard(text) {
+    var textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert('Copied successfully!')
+}
 
+// Chame a função com o texto que deseja copiar
+copyToClipboard((webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken());
